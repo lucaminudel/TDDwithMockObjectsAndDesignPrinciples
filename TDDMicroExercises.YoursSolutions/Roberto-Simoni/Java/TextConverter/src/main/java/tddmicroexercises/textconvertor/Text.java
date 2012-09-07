c7017fbb6fc41c224de9d4fed874c9fd0c8c409e
Roberto Simoni <roberto.simoni@yahoo.it>
2012-09-07 15:10:50 +0200

    Text works on File(s) too

--- TDDMicroExercises.YoursSolutions/Roberto-Simoni/Java/TextConverter/src/main/java/tddmicroexercises/textconvertor/Text.java
+++ TDDMicroExercises.YoursSolutions/Roberto-Simoni/Java/TextConverter/src/main/java/tddmicroexercises/textconvertor/Text.java
@@ -1,6 +1,9 @@
 package tddmicroexercises.textconvertor;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
 import java.io.StringReader;
@@ -15,10 +18,14 @@ public class Text {
 		this(new StringReader(text));
 	}
 
+	public Text(File file) throws FileNotFoundException {
+		this(new FileReader(file));
+	}
+
 	public Text(Reader reader) {
 		this.reader = reader;
 	}
-
+	
 	public String toHtml() {
 		try {
 			StringBuilder builder = new StringBuilder();
--- TDDMicroExercises.YoursSolutions/Roberto-Simoni/Java/TextConverter/src/test/java/tddmicroexercises/textconvertor/TextTest.java
+++ TDDMicroExercises.YoursSolutions/Roberto-Simoni/Java/TextConverter/src/test/java/tddmicroexercises/textconvertor/TextTest.java
@@ -2,6 +2,11 @@ package tddmicroexercises.textconvertor;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang.SystemUtils;
 import org.junit.Test;
 
 public class TextTest {
@@ -13,6 +18,19 @@ public class TextTest {
 
 	@Test public void toHtml_useHtmlBreakInPlaceOfNewLines() {
 		assertEquals("hello<br/>mum&amp;dad<br/>less&lt;than", new Text("hello\nmum&dad\nless<than").toHtml());
+	}
+
+	@Test public void worksOnFilesToo() throws IOException {
+		File file = aNewFileContaining("hello\nmum&dad\nless<than");
+		
+		assertEquals("hello<br/>mum&amp;dad<br/>less&lt;than", new Text(file).toHtml());
+	}
+
+	private File aNewFileContaining(String content) throws IOException {
+		File file = new File(SystemUtils.JAVA_IO_TMPDIR, "textconverter-data.txt");
+		if (file.exists()) file.delete();
+		FileUtils.writeStringToFile(file, content);
+		return file;
 	}
 
 }
