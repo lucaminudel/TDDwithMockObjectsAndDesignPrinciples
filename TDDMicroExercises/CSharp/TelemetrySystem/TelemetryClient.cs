12fa5822096df10c0ddc78b45fb146ef925c7fc4
Luca Minudel <mymailgateway-github@yahoo.it>
2013-03-20 17:18:19 +0100

    Update: added comments to make exercise scope more clear.

--- TDDMicroExercises/CSharp/TelemetrySystem/TelemetryClient.cs
+++ TDDMicroExercises/CSharp/TelemetrySystem/TelemetryClient.cs
@@ -4,12 +4,18 @@ namespace TDDMicroExercises.TelemetrySystem
 {
 	public class TelemetryClient
 	{
+        //
+        // The communication with the server is simulated in this implementation.
+        // Because the focus of the exercise is on the other class.
+        //
+
 		public const string DiagnosticMessage = "AT#UD";
 
 		private bool _onlineStatus;
-		private string _diagnosticMessageResult = string.Empty;
+		private bool _diagnosticMessageJustSent = false;
 
-		private readonly Random _connectionEventsSimulator = new Random(42);
+        private readonly Random _connectionEventsSimulator = new Random(42);
+        private readonly Random _randomMessageSimulator = new Random(42);
 
 		public bool OnlineStatus
 		{
@@ -23,7 +29,7 @@ namespace TDDMicroExercises.TelemetrySystem
 				throw new ArgumentNullException();
 			}
 
-			// simulate the operation on a real modem
+			// Fake the connection with 80% chances of success
 			bool success = _connectionEventsSimulator.Next(1, 10) <= 8;
 
 			_onlineStatus = success;
@@ -42,48 +48,52 @@ namespace TDDMicroExercises.TelemetrySystem
 				throw new ArgumentNullException();
 			}
 
-			if (message == DiagnosticMessage)
+            // The simulation of Send() actually just remember if the last message sent was a diagnostic message.
+            // This information will be used to simulate the Receive(). Indeed there is no real server listening.
+            if (message == DiagnosticMessage)
 			{
-				// simulate a status report
-				_diagnosticMessageResult =
-					  "LAST TX rate................ 100 MBPS\r\n"
-					+ "HIGHEST TX rate............. 100 MBPS\r\n"
-					+ "LAST RX rate................ 100 MBPS\r\n"
-					+ "HIGHEST RX rate............. 100 MBPS\r\n"
-					+ "BIT RATE.................... 100000000\r\n"
-					+ "WORD LEN.................... 16\r\n"
-					+ "WORD/FRAME.................. 511\r\n"
-					+ "BITS/FRAME.................. 8192\r\n"
-					+ "MODULATION TYPE............. PCM/FM\r\n"
-					+ "TX Digital Los.............. 0.75\r\n"
-					+ "RX Digital Los.............. 0.10\r\n"
-					+ "BEP Test.................... -5\r\n"
-					+ "Local Rtrn Count............ 00\r\n"
-					+ "Remote Rtrn Count........... 00";
+			    _diagnosticMessageJustSent = true;
 
-				return;
+			}
+			else
+			{
+			    _diagnosticMessageJustSent = false;
 			}
 	
-			// here should go the real Send operation
 		}
 
 		public string Receive()
 		{
 			string message;
 
-			if (string.IsNullOrEmpty(_diagnosticMessageResult) == false)
+            if (_diagnosticMessageJustSent)
 			{
-				message = _diagnosticMessageResult;
-				_diagnosticMessageResult = string.Empty;
+                // Simulate the reception of the diagnostic message
+                message = "LAST TX rate................ 100 MBPS\r\n"
+                    + "HIGHEST TX rate............. 100 MBPS\r\n"
+                    + "LAST RX rate................ 100 MBPS\r\n"
+                    + "HIGHEST RX rate............. 100 MBPS\r\n"
+                    + "BIT RATE.................... 100000000\r\n"
+                    + "WORD LEN.................... 16\r\n"
+                    + "WORD/FRAME.................. 511\r\n"
+                    + "BITS/FRAME.................. 8192\r\n"
+                    + "MODULATION TYPE............. PCM/FM\r\n"
+                    + "TX Digital Los.............. 0.75\r\n"
+                    + "RX Digital Los.............. 0.10\r\n"
+                    + "BEP Test.................... -5\r\n"
+                    + "Local Rtrn Count............ 00\r\n"
+                    + "Remote Rtrn Count........... 00";
+
+                _diagnosticMessageJustSent = false;
 			} 
 			else
 			{                
-				// simulate a received message
+				// Simulate the reception of a response message returning a random message.
 				message = string.Empty;
-				int messageLenght = _connectionEventsSimulator.Next(50, 110);
-				for(int i = messageLenght; i >=0; --i)
+                int messageLenght = _randomMessageSimulator.Next(50, 110);
+				for(int i = messageLenght; i > 0; --i)
 				{
-					message += (char)_connectionEventsSimulator.Next(40, 126);
+                    message += (char)_randomMessageSimulator.Next(40, 126);
 				}
 			}
 
