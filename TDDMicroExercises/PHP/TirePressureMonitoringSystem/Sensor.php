dd9b1896a21692bb0370ad097fc96d49a127ab90
Arthur Almeida <arthur.almeidapereira@gmail.com>
2013-05-11 02:29:09 -0300

    implementing Tire Pressure Monitoring System PHP version

---
+++ TDDMicroExercises/PHP/TirePressureMonitoringSystem/Sensor.php
@@ -0,0 +1,20 @@
+<?php
+
+
+class Sensor 
+{
+	const OFFSET = 16;
+
+	public static function SamplePressure()
+	{
+		// placeholder implementation that simulate a real sensor in a real tire
+		$pressureTelemetryValue = floor(6 * rand() * rand());
+		return $pressureTelemetryValue;
+	}
+
+	public function popNextPressurePsiValue() 
+	{
+		$pressureTelemetryValue = self::SamplePressure();
+		return self::OFFSET + $pressureTelemetryValue;
+	}
+}
