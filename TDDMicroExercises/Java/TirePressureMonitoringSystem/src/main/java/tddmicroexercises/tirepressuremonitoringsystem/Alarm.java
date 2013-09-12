45f40e5c04d3d1439ce04875a977d70eeddef052
Luca Minudel <lminudel@thoughtworks.com>
2013-09-12 17:12:38 +0100

    TirePressureMonitoringSystem/Java: deleted unnecessary code and synchronised the proposed solution with changes in the original exercise.

--- TDDMicroExercises/Java/TirePressureMonitoringSystem/src/main/java/tddmicroexercises/tirepressuremonitoringsystem/Alarm.java
+++ TDDMicroExercises/Java/TirePressureMonitoringSystem/src/main/java/tddmicroexercises/tirepressuremonitoringsystem/Alarm.java
@@ -8,7 +8,6 @@ public class Alarm
     Sensor sensor = new Sensor();
 
     boolean alarmOn = false;
-    private long alarmCount = 0;
 
     public void check()
     {
@@ -17,7 +16,6 @@ public class Alarm
         if (psiPressureValue < LowPressureThreshold || HighPressureThreshold < psiPressureValue)
         {
             alarmOn = true;
-            alarmCount += 1;
         }
     }
 
