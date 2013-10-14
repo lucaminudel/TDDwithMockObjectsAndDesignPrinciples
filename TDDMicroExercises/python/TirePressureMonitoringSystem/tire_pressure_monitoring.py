041f2983ea70085d8ade75bf3bddb9bb45565811
Luca Minudel <lminudel@thoughtworks.com>
2013-10-14 09:27:47 +0100

    TirePressureMonitoringSystem/Python: replaced double underscore with single underscore to follow Python 3 conventions.

--- TDDMicroExercises/python/TirePressureMonitoringSystem/tire_pressure_monitoring.py
+++ TDDMicroExercises/python/TirePressureMonitoringSystem/tire_pressure_monitoring.py
@@ -5,11 +5,11 @@ class Sensor(object):
     # The reading of the pressure value from the sensor is simulated in this implementation.
     # Because the focus of the exercise is on the other class.
 
-    __OFFSET = 16
+    _OFFSET = 16
         
     def pop_next_pressure_psi_value(self):
         pressure_telemetry_value = self.sample_pressure()
-        return Sensor.__OFFSET + pressure_telemetry_value
+        return Sensor._OFFSET + pressure_telemetry_value
 
     @staticmethod
     def sample_pressure():
