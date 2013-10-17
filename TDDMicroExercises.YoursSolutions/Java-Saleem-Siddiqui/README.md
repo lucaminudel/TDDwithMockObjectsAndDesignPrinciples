962b2948bcb1d7d3492dc19f43551ea7fb67dafe
saleem <saleem@saleem-siddiqui.com>
2013-10-18 01:46:42 +0200

    @saleem: initial commit of TirePressurMonitoringSystem + gradle.build + .gitignore

---
+++ TDDMicroExercises.YoursSolutions/Java-Saleem-Siddiqui/README.md
@@ -0,0 +1,7 @@
+1. Tire Pressure Monitoring System
+2. Text Converter
+
+Use Mockito
+  If time permits, hand-write a mock class with introspective behavior, and compare it with the Mockito solution
+
+Use Gradle
---
+++ TDDMicroExercises.YoursSolutions/Java-Saleem-Siddiqui/src/main/java/tddmicroexercises/tirepressuremonitoringsystem/Alarm.java
@@ -0,0 +1,26 @@
+package tddmicroexercises.tirepressuremonitoringsystem;
+
+public class Alarm
+{
+    private final double LowPressureThreshold = 17;
+    private final double HighPressureThreshold = 21;
+
+    Sensor sensor = new Sensor();
+
+    boolean alarmOn = false;
+
+    public void check()
+    {
+        double psiPressureValue = sensor.popNextPressurePsiValue();
+
+        if (psiPressureValue < LowPressureThreshold || HighPressureThreshold < psiPressureValue)
+	    {
+		alarmOn = true;
+	    }
+    }
+
+    public boolean isAlarmOn()
+    {
+        return alarmOn; 
+    }
+}
