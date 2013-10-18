696eb473b299cddebccaeda2ae67c8159ccae275
saleem <saleem@saleem-siddiqui.com>
2013-10-18 02:23:21 +0200

    @saleem: new broken test: adding interface ISensor using TDD. This commit at the 'Red' stage of 'RGR'

--- TDDMicroExercises.YoursSolutions/Java-Saleem-Siddiqui/src/test/java/tddmicroexercises/tirepressuremonitoringsystem/AlarmTest.java
+++ TDDMicroExercises.YoursSolutions/Java-Saleem-Siddiqui/src/test/java/tddmicroexercises/tirepressuremonitoringsystem/AlarmTest.java
@@ -3,10 +3,17 @@ package tddmicroexercises.tirepressuremonitoringsystem;
 import org.junit.Test;
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static java.lang.Boolean.FALSE;
+import static org.mockito.Mockito.mock;
 
 public class AlarmTest {
     @Test
+    public void canCreatAlarmWithGivenSensor() {
+	new Alarm(mock(ISensor.class));
+    }
+
+    @Test
     public void aNewAlarmIsOff() {
-        assertThat((new Alarm()).isAlarmOn(), is(Boolean.FALSE));
+        assertThat((new Alarm()).isAlarmOn(), is(FALSE));
     }
 }
