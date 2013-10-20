29c03cefab9ea202ce768c5dcdd627a464518352
saleem <saleem@saleem-siddiqui.com>
2013-10-20 20:36:56 +0200

    @saleem: Added unit test to verify that the class-under-test, UnicodeFileToHtmlTextConverter, does call the static method of StringEscapeUtils. Used PowerMockito to accomplish this. No refactoring of UnicodeFileToHtmlTextConverter performed thus far.

--- TDDMicroExercises.YoursSolutions/Java-Saleem-Siddiqui/src/test/java/tddmicroexercises/textconverter/UnicodeFileToHtmlTextConverterTest.java
+++ TDDMicroExercises.YoursSolutions/Java-Saleem-Siddiqui/src/test/java/tddmicroexercises/textconverter/UnicodeFileToHtmlTextConverterTest.java
@@ -6,9 +6,19 @@ import java.io.BufferedWriter;
 import org.junit.Before;
 import org.junit.After;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.apache.commons.lang3.StringEscapeUtils;
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.powermock.api.mockito.PowerMockito.*;
+import static org.mockito.Mockito.anyString;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.core.classloader.annotations.SuppressStaticInitializationFor;
+import org.powermock.modules.junit4.PowerMockRunner;
 
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({UnicodeFileToHtmlTextConverter.class, StringEscapeUtils.class})
 public class UnicodeFileToHtmlTextConverterTest {
     private static final String FILE_NAME = "temp";
     private static final String FILE_EXT = "test";
@@ -41,5 +51,19 @@ public class UnicodeFileToHtmlTextConverterTest {
     public void converterReturnsEmptyStringForEmptyFile() throws Exception {
 	UnicodeFileToHtmlTextConverter converter = new UnicodeFileToHtmlTextConverter(tempInputFile.getAbsolutePath());
 	assertThat(converter.convertToHtml(), is(""));
+    }
+
+    @Test
+    public void converterShouldCallStaticMethodInLibraryForEachLineInInputFile() throws Exception {
+	// Arrange
+	mockStatic(StringEscapeUtils.class);
+	when(StringEscapeUtils.escapeHtml4(anyString())).thenReturn("return value does not matter");
+	writeToTempInputFile("Hello World");
+	UnicodeFileToHtmlTextConverter converter = new UnicodeFileToHtmlTextConverter(tempInputFile.getAbsolutePath());
+	// Act
+	converter.convertToHtml();
+	// Assert
+	verifyStatic();
+	StringEscapeUtils.escapeHtml4("Hello World");
     }
 }
