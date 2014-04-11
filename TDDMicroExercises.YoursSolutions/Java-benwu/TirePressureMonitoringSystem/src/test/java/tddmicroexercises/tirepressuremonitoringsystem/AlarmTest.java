7466f18aa2d9d7ee2883c94944de6c4ab85fcf1f
Ben Wu <wubin28@gmail.com>
2014-04-11 20:06:42 +0800

    Mocked Sensor in test given_psi_pressure_value_is_less_than_17_when_check_alarm_then_alarm_is_on().

--- TDDMicroExercises.YoursSolutions/Java-benwu/TirePressureMonitoringSystem/src/test/java/tddmicroexercises/tirepressuremonitoringsystem/AlarmTest.java
+++ TDDMicroExercises.YoursSolutions/Java-benwu/TirePressureMonitoringSystem/src/test/java/tddmicroexercises/tirepressuremonitoringsystem/AlarmTest.java
@@ -17,6 +17,12 @@ public class AlarmTest {
 
     @Test
     public void given_psi_pressure_value_is_less_than_17_when_check_alarm_then_alarm_is_on() {
+        Sensor sensor = mock(Sensor.class);
+        when(sensor.popNextPressurePsiValue()).thenReturn(16.0);
+        Alarm alarm = new Alarm(sensor);
+
+        alarm.check();
+
         assertThat(alarm.isAlarmOn()).isEqualTo(true);
     }
 }
