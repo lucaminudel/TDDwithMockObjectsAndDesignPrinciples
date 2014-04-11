fd2ca7bd9247f758d1e11ba75f98678f89cd7beb
Ben Wu <wubin28@gmail.com>
2014-04-11 22:05:02 +0800

    Made test when_pop_next_pressure_psi_value_then_get_characterization() passed.

--- TDDMicroExercises.YoursSolutions/Java-benwu/TirePressureMonitoringSystem/src/test/java/tddmicroexercises/tirepressuremonitoringsystem/SensorTest.java
+++ TDDMicroExercises.YoursSolutions/Java-benwu/TirePressureMonitoringSystem/src/test/java/tddmicroexercises/tirepressuremonitoringsystem/SensorTest.java
@@ -2,12 +2,16 @@ package tddmicroexercises.tirepressuremonitoringsystem;
 
 import org.junit.Test;
 
+import static org.fest.assertions.api.Assertions.assertThat;
+
 /**
  * Created by ben on 14-4-11.
  */
 public class SensorTest {
     @Test
     public void when_pop_next_pressure_psi_value_then_get_characterization() {
-        assertThat(sensor.popNextPressurePsiValue()).isEqualTo(0);
+        Sensor sensor = new Sensor();
+        sensor.popNextPressurePsiValue();
+        assertThat(sensor.popNextPressurePsiValue()).isGreaterThan(16.0);
     }
 }
