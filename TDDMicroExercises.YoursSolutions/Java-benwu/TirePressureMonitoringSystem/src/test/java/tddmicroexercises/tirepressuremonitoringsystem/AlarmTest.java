71a90063d54f9058f3a602c6c863750f00669332
Ben Wu <wubin28@gmail.com>
2014-04-11 15:38:33 +0800

    Made test when_check_alarm_then_get_characterization() passed.

--- TDDMicroExercises.YoursSolutions/Java-benwu/TirePressureMonitoringSystem/src/test/java/tddmicroexercises/tirepressuremonitoringsystem/AlarmTest.java
+++ TDDMicroExercises.YoursSolutions/Java-benwu/TirePressureMonitoringSystem/src/test/java/tddmicroexercises/tirepressuremonitoringsystem/AlarmTest.java
@@ -2,6 +2,8 @@ package tddmicroexercises.tirepressuremonitoringsystem;
 
 import org.junit.Test;
 
+import static org.fest.assertions.api.Assertions.assertThat;
+
 /**
  * Created by ben on 14-4-11.
  */
@@ -10,6 +12,6 @@ public class AlarmTest {
     public void when_check_alarm_then_get_characterization() {
         Alarm alarm = new Alarm();
         alarm.check();
-        assertThat(alarm.isAlarmOn()).isEqualTo(true);
+        assertThat(alarm.isAlarmOn()).isEqualTo(false);
     }
 }
