72d091c5e66a13a851597a99e2ed4be4d793fed3
Ben Wu <wubin28@gmail.com>
2014-04-15 08:30:49 +0800

    Added an assertion for test WHEN_check_pressure_value_in_alarm_THEN_get_characterization_of_whether_alarm_is_on().

---
+++ TDDMicroExercises.YoursSolutions/CSharp-benwu/TirePressureMonitoringSystem.Tests/AlarmTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace TDDMicroExercises.TirePressureMonitoringSystem.Tests
+{
+    [TestFixture]
+    public class AlarmTest
+    {
+        [Test]
+        public void WHEN_check_pressure_value_in_alarm_THEN_get_characterization_of_whether_alarm_is_on()
+        {
+            Assert.AreEqual(true, alarm.AlarmOn());
+        }
+    }
+}
