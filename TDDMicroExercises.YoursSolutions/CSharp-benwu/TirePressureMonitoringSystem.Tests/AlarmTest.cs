5b0e65249a74a117e28cd8c5aa500d7c3813a7cc
Ben Wu <wubin28@gmail.com>
2014-04-16 22:28:15 +0800

    Added test GIVEN_pressure_psi_value_is_22_WHEN_check_pressure_value_in_alarm_THEN_alarm_is_on() and it was passed.

--- TDDMicroExercises.YoursSolutions/CSharp-benwu/TirePressureMonitoringSystem.Tests/AlarmTest.cs
+++ TDDMicroExercises.YoursSolutions/CSharp-benwu/TirePressureMonitoringSystem.Tests/AlarmTest.cs
@@ -65,5 +65,24 @@ namespace TDDMicroExercises.TirePressureMonitoringSystem.Tests
             Assert.IsFalse(alarm.AlarmOn);
             sensor.Verify(s => s.PopNextPressurePsiValue());
         }
+
+        [Test]
+        public void GIVEN_pressure_psi_value_is_22_WHEN_check_pressure_value_in_alarm_THEN_alarm_is_on()
+        {
+            // Given
+            var sensor = new Mock<ISensor>();
+            var alarm = new Alarm(sensor.Object);
+
+            sensor
+                .Setup(s => s.PopNextPressurePsiValue())
+                .Returns(22.0);
+
+            // When
+            alarm.Check();
+
+            // Then
+            Assert.IsTrue(alarm.AlarmOn);
+            sensor.Verify(s => s.PopNextPressurePsiValue());
+        }
     }
 }
