6087397c9337373a2c47694d7693cc249e846ec7
Ben Wu <wubin28@gmail.com>
2014-04-17 21:47:46 +0800

    Added test should_convert_ampersand() in test class UnicodeFileToHtmlTextConverterTest.

--- TDDMicroExercises.YoursSolutions/CSharp-benwu/TirePressureMonitoringSystem.Tests/AlarmTest.cs
+++ TDDMicroExercises.YoursSolutions/CSharp-benwu/TirePressureMonitoringSystem.Tests/AlarmTest.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Moq;
+﻿using Moq;
 using NUnit.Framework;
 
 namespace TDDMicroExercises.TirePressureMonitoringSystem.Tests
---
+++ TDDMicroExercises.YoursSolutions/CSharp-benwu/UnicodeFileToHtmlTextConverter.Tests/UnicodeFileToHtmlTextConverterTest.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace TDDMicroExercises.UnicodeFileToHtmlTextConverter.Tests
+{
+    [TestFixture]
+    public class UnicodeFileToHtmlTextConverterTest
+    {
+        [Test]
+        public void should_convert_ampersand()
+        {
+            TestingUnicodeFileToHtmlTextConverter testingUnicodeFileToHtmlTextConverter = new TestingUnicodeFileToHtmlTextConverter("Copy & Paste");
+            Assert.AreEqual("Copy &amp; Paste<br />", testingUnicodeFileToHtmlTextConverter.ConvertToHtml());
+        }
+    }
+}
