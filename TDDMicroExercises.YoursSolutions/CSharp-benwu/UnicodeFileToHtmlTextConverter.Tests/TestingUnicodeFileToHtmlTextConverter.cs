f7ac0dc3131fe7e53c5341ac3d8b13f67df89aba
Ben Wu <wubin28@gmail.com>
2014-04-17 21:55:34 +0800

    Created class TestingUnicodeFileToHtmlTextConverter which is the subclass of UnicodeFileToHtmlTextConverter.

---
+++ TDDMicroExercises.YoursSolutions/CSharp-benwu/UnicodeFileToHtmlTextConverter.Tests/TestingUnicodeFileToHtmlTextConverter.cs
@@ -0,0 +1,9 @@
+﻿namespace TDDMicroExercises.UnicodeFileToHtmlTextConverter.Tests
+{
+    public class TestingUnicodeFileToHtmlTextConverter : UnicodeFileToHtmlTextConverter
+    {
+        public TestingUnicodeFileToHtmlTextConverter(string strToBeConverted) : base (strToBeConverted)
+        {
+        }
+    }
+}
