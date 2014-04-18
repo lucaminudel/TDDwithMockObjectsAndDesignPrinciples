26d90b226eecf053d0b799454946d272497ce326
Ben Wu <wubin28@gmail.com>
2014-04-18 10:36:24 +0800

    Added test GIVEN_dispense_a_ticket_WHEN_dispense_a_new_ticket_at_another_machine_THEN_turn_number_of_the_new_ticket_is_greater() and made it passed.

--- TDDMicroExercises.YoursSolutions/CSharp-benwu/TurnTicketDispenser.Tests/TicketDispenserTest.cs
+++ TDDMicroExercises.YoursSolutions/CSharp-benwu/TurnTicketDispenser.Tests/TicketDispenserTest.cs
@@ -6,12 +6,25 @@ namespace TDDMicroExercises.TurnTicketDispenser.Tests
     public class TicketDispenserTest
     {
         [Test]
-        public void GIVEN_dispense_a_ticket_WHEN_dispense_a_new_ticket_THEN_turn_number_of_the_new_ticket_is_greater()
+        public void GIVEN_dispense_a_ticket_WHEN_dispense_a_new_ticket_at_the_same_machine_THEN_turn_number_of_the_new_ticket_is_greater()
         {
-            TicketDispenser tickDispenser = new TicketDispenser();
-            TurnTicket oldTicket = tickDispenser.GetTurnTicket();
-            TurnTicket newTicket = tickDispenser.GetTurnTicket();
+            TicketDispenser ticketDispenser = new TicketDispenser();
+            TurnTicket oldTicket = ticketDispenser.GetTurnTicket();
+            TurnTicket newTicket = ticketDispenser.GetTurnTicket();
             Assert.Greater(newTicket.TurnNumber, oldTicket.TurnNumber);
         }
+
+        [Test]
+        public void GIVEN_dispense_a_ticket_WHEN_dispense_a_new_ticket_at_another_machine_THEN_turn_number_of_the_new_ticket_is_greater()
+        {
+            TicketDispenser ticketDispenser = new TicketDispenser();
+            TurnTicket oldTicket = ticketDispenser.GetTurnTicket();
+
+            TicketDispenser anotherTicketDispenser = new TicketDispenser();
+            TurnTicket newTicket = anotherTicketDispenser.GetTurnTicket();
+
+            Assert.Greater(newTicket.TurnNumber, oldTicket.TurnNumber);
+        }
+
     }
 }
