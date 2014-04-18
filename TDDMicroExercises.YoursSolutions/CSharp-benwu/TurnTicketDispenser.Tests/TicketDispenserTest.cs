504e104e91edd754da2d35bdb71653cd736ba5d3
Ben Wu <wubin28@gmail.com>
2014-04-18 09:54:20 +0800

    Added test GIVEN_dispense_a_ticket_WHEN_check_ticket_turn_number_THEN_get_characterization().

---
+++ TDDMicroExercises.YoursSolutions/CSharp-benwu/TurnTicketDispenser.Tests/TicketDispenserTest.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace TDDMicroExercises.TurnTicketDispenser.Tests
+{
+    [TestFixture]
+    public class TicketDispenserTest
+    {
+        [Test]
+        public void GIVEN_dispense_a_ticket_WHEN_check_ticket_turn_number_THEN_get_characterization()
+        {
+            Assert.AreEqual(0, ticket.TurnNumber);
+        }
+    }
+}
