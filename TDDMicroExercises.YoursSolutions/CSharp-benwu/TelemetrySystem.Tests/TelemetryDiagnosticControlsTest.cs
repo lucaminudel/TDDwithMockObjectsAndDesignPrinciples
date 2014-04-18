d8dfa2d9f594e66e53e3721fa5d895575434141c
Ben Wu <wubin28@gmail.com>
2014-04-18 20:03:28 +0800

    Renamed the test to be WHEN_check_Transmision_THEN_diagnostic_request_should_be_sent_and_message_should_be_got() and mocked class TelemetryClient in it.

--- TDDMicroExercises.YoursSolutions/CSharp-benwu/TelemetrySystem.Tests/TelemetryDiagnosticControlsTest.cs
+++ TDDMicroExercises.YoursSolutions/CSharp-benwu/TelemetrySystem.Tests/TelemetryDiagnosticControlsTest.cs
@@ -1,3 +1,4 @@
+using Moq;
 using NUnit.Framework;
 
 namespace TDDMicroExercises.TelemetrySystem.Tests
@@ -6,9 +7,18 @@ namespace TDDMicroExercises.TelemetrySystem.Tests
     public class TelemetryDiagnosticControlsTest
     {
         [Test]
-        public void WHEN_check_Transmision_THEN_get_characterization()
+        public void WHEN_check_Transmision_THEN_diagnostic_request_should_be_sent_and_message_should_be_got()
         {
-            TelemetryDiagnosticControls telementryDiagnosticControls = new TelemetryDiagnosticControls();
+            var telemetryClient = new Mock<TelemetryClient>();
+
+            telemetryClient
+                .Setup(t => t.Send(TelemetryClient.DiagnosticMessage));
+
+            telemetryClient
+                .Setup(t => t.Receive())
+                .Returns("LAST TX rate................ 100 MBPS\r\n");
+
+            TelemetryDiagnosticControls telementryDiagnosticControls = new TelemetryDiagnosticControls(telemetryClient.Object);
             telementryDiagnosticControls.CheckTransmission();
             Assert.AreEqual("", telementryDiagnosticControls.DiagnosticInfo);
         }
