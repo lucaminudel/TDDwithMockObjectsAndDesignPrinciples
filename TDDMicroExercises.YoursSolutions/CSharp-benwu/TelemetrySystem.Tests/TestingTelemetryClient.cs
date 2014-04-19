fe3267a8d2439f63c360de8be12c45ed55b7ed75
Ben Wu <wubin28@gmail.com>
2014-04-19 14:58:09 +0800

    Implemented the constructor of TestingTelemetryClient for injecting the three random numbers.

--- TDDMicroExercises.YoursSolutions/CSharp-benwu/TelemetrySystem.Tests/TestingTelemetryClient.cs
+++ TDDMicroExercises.YoursSolutions/CSharp-benwu/TelemetrySystem.Tests/TestingTelemetryClient.cs
@@ -2,5 +2,15 @@ namespace TDDMicroExercises.TelemetrySystem.Tests
 {
     public class TestingTelemetryClient : TelemetryClient
     {
+        private int _connectionEventsRandomNumberFrom1To10;
+        private int _messageRandomNumberFrom50To110;
+        private int _messageRandomNumberFrom40To126;
+
+        public TestingTelemetryClient(int connectionEventsRandomNumberFrom1To10, int messageRandomNumberFrom50To110, int messageRandomNumberFrom40To126)
+        {
+            this._connectionEventsRandomNumberFrom1To10 = connectionEventsRandomNumberFrom1To10;
+            this._messageRandomNumberFrom50To110 = messageRandomNumberFrom50To110;
+            this._messageRandomNumberFrom40To126 = messageRandomNumberFrom40To126;
+        }
     }
 }
