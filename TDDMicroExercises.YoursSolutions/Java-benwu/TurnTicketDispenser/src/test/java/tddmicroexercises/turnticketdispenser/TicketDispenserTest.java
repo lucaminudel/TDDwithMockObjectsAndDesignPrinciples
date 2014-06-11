6ce58d0dfefb7928deb8673cbd3eec555a0ec636
Ben Wu <wubin28@gmail.com>
2014-06-11 23:24:51 +0800

    Finished TODO: a new ticket should have the turn number subsequent to the previous ticket from another dispenser.

--- TDDMicroExercises.YoursSolutions/Java-benwu/TurnTicketDispenser/src/test/java/tddmicroexercises/turnticketdispenser/TicketDispenserTest.java
+++ TDDMicroExercises.YoursSolutions/Java-benwu/TurnTicketDispenser/src/test/java/tddmicroexercises/turnticketdispenser/TicketDispenserTest.java
@@ -20,6 +20,20 @@ public class TicketDispenserTest {
     }
 
     // TODO-acceptance-test: a new ticket should have the turn number subsequent to the previous ticket from another dispenser
+    @Test
+    public void a_new_ticket_should_have_the_turn_number_subsequent_to_the_previous_ticket_from_another_dispenser() {
+        // Arrange
+        TicketDispenser dispenser = new TicketDispenser();
+        TicketDispenser anotherDispenser = new TicketDispenser();
+
+        // Act
+        TurnTicket previousTicket = dispenser.getTurnTicket();
+        TurnTicket newTicketFromAnotherDispenser = anotherDispenser.getTurnTicket();
+
+        // Assert
+        assertTrue(newTicketFromAnotherDispenser.getTurnNumber() > previousTicket.getTurnNumber());
+    }
+
     // TODO-acceptance-test: after ticket 10 come ticket 11
 
 }
