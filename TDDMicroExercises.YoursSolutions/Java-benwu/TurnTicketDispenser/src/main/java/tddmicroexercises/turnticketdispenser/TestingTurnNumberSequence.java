fcca28eb7a653d2d6beaab33f3659bb9a0a088f2
Ben Wu <wubin28@gmail.com>
2014-06-11 23:35:41 +0800

    Created and implemented method TestingTurnNumberSequence.setCurrentTurnNumber().

--- TDDMicroExercises.YoursSolutions/Java-benwu/TurnTicketDispenser/src/main/java/tddmicroexercises/turnticketdispenser/TestingTurnNumberSequence.java
+++ TDDMicroExercises.YoursSolutions/Java-benwu/TurnTicketDispenser/src/main/java/tddmicroexercises/turnticketdispenser/TestingTurnNumberSequence.java
@@ -4,4 +4,7 @@ package tddmicroexercises.turnticketdispenser;
  * Created by benwu on 14-6-11.
  */
 public class TestingTurnNumberSequence extends TurnNumberSequence {
+    public void setCurrentTurnNumber(int currentTurnNumber) {
+        super.turnNumber = currentTurnNumber;
+    }
 }
