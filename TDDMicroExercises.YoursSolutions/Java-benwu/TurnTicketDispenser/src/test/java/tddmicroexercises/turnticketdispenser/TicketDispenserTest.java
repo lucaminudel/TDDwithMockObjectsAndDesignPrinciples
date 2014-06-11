254270845e5390c8ae55a426354bf6fbd92b0430
Ben Wu <wubin28@gmail.com>
2014-06-11 23:16:57 +0800

    Added 3 acceptance test TODOs.

--- TDDMicroExercises.YoursSolutions/Java-benwu/TurnTicketDispenser/src/test/java/tddmicroexercises/turnticketdispenser/TicketDispenserTest.java
+++ TDDMicroExercises.YoursSolutions/Java-benwu/TurnTicketDispenser/src/test/java/tddmicroexercises/turnticketdispenser/TicketDispenserTest.java
@@ -9,4 +9,9 @@ public class TicketDispenserTest {
     public void a_trivial_test() {
         assertEquals(5, 2 + 3);
     }
+
+    // TODO-acceptance-test: a new ticket should have the turn number subsequent to the previous ticket
+    // TODO-acceptance-test: a new ticket should have the turn number subsequent to the previous ticket from another dispenser
+    // TODO-acceptance-test: after ticket 10 come ticket 11
+
 }
