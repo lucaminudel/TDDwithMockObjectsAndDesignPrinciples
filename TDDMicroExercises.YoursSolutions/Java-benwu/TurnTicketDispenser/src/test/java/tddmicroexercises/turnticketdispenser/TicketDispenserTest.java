73e265adc611e698ff83f4398ed3556db1e753ee
Ben Wu <wubin28@gmail.com>
2014-06-11 23:12:01 +0800

    Created the test class TicketDispenserTest and added a trivial test.

---
+++ TDDMicroExercises.YoursSolutions/Java-benwu/TurnTicketDispenser/src/test/java/tddmicroexercises/turnticketdispenser/TicketDispenserTest.java
@@ -0,0 +1,12 @@
+package tddmicroexercises.turnticketdispenser;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TicketDispenserTest {
+    @Test
+    public void a_trivial_test() {
+        assertEquals(5, 2 + 3);
+    }
+}
