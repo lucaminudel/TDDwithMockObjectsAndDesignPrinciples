8927ebcf3888b1bbd0f130a509447f697db20c03
Ben Wu <wubin28@gmail.com>
2014-06-11 23:15:20 +0800

    Added TODO: Depending on a static method violates the Dependency Inversion Principle and Open-Closed Principle.

--- TDDMicroExercises.YoursSolutions/Java-benwu/TurnTicketDispenser/src/main/java/tddmicroexercises/turnticketdispenser/TicketDispenser.java
+++ TDDMicroExercises.YoursSolutions/Java-benwu/TurnTicketDispenser/src/main/java/tddmicroexercises/turnticketdispenser/TicketDispenser.java
@@ -4,6 +4,7 @@ public class TicketDispenser
 {
     public TurnTicket getTurnTicket()
     {
+        // TODO: Depending on a static method violates the Dependency Inversion Principle and Open-Closed Principle.
         int newTurnNumber = TurnNumberSequence.getNextTurnNumber();
         TurnTicket newTurnTicket = new TurnTicket(newTurnNumber);
 
