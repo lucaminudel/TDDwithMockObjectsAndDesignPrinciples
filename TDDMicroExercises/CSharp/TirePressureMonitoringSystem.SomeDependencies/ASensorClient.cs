2f51ac9d890739e102a50fc8d75000db1d730c88
Luca Minudel <mymailgateway-github@yahoo.it>
2017-07-07 16:09:25 +0200

    C# Project updated to .NET Framework 4.5.2, and NUnit framework 3.7.1. Added client classes to the Tire Pressure Monitoring exercise, to better simulate real-life scenarios with dependencies on classes refactored.

---
+++ TDDMicroExercises/CSharp/TirePressureMonitoringSystem.SomeDependencies/ASensorClient.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace TDDMicroExercises.TirePressureMonitoringSystem.SomeDependencies
+{
+    public class ASensorClient      
+    {
+        // A class with the only goal of simulating a dependency on Sensor
+        // that has impact on the refactoring.
+
+        public ASensorClient()
+        {
+            Sensor sensor = new Sensor();
+
+			double value = sensor.PopNextPressurePsiValue();
+			value = sensor.PopNextPressurePsiValue();
+			value = sensor.PopNextPressurePsiValue();
+			value = sensor.PopNextPressurePsiValue();
+		}
+    }
+}
---
+++ TDDMicroExercises/CSharp/TirePressureMonitoringSystem.SomeDependencies/AnAlarmClient3.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace TDDMicroExercises.TirePressureMonitoringSystem.SomeDependencies
+{
+    public class AnAlarmClient3
+    {
+        // A class with the only goal of simulating a dependency on Alart
+        // that has impact on the refactoring.
+
+        private Alarm _anAlarm;
+
+        public AnAlarmClient3()
+        {
+            _anAlarm = new Alarm();
+        }
+
+        public void DoSomething() 
+        {
+			_anAlarm.Check();          
+        }
+
+		public void DoSomethingElse()
+		{
+			bool isAlarmOn = _anAlarm.AlarmOn;
+		}
+    }
+}
