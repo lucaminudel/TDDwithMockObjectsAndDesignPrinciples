63be80c7fde7d47af428266eecff79b88ee8b65a
Luca Minudel <mymailgateway-github@yahoo.it>
2017-08-09 16:32:08 +0100

    Fixed typo in comments

--- TDDMicroExercises/CSharp/TirePressureMonitoringSystem.SomeDependencies/AnAlarmClient3.cs
+++ TDDMicroExercises/CSharp/TirePressureMonitoringSystem.SomeDependencies/AnAlarmClient3.cs
@@ -3,7 +3,7 @@ namespace TDDMicroExercises.TirePressureMonitoringSystem.SomeDependencies
 {
     public class AnAlarmClient3
     {
-        // A class with the only goal of simulating a dependency on Alart
+        // A class with the only goal of simulating a dependency on Alert
         // that has impact on the refactoring.
 
         private Alarm _anAlarm;
--- TDDMicroExercises/Java/TirePressureMonitoringSystem/src/main/java/tddmicroexercises/tirepressuremonitoringsystem/somedependencies/AnAlarmClient3.java
+++ TDDMicroExercises/Java/TirePressureMonitoringSystem/src/main/java/tddmicroexercises/tirepressuremonitoringsystem/somedependencies/AnAlarmClient3.java
@@ -4,7 +4,7 @@ import tddmicroexercises.tirepressuremonitoringsystem.Alarm;
 
 public class AnAlarmClient3
 {
-    // A class with the only goal of simulating a dependency on Alart
+    // A class with the only goal of simulating a dependency on Alert
     // that has impact on the refactoring.
 
     private Alarm _anAlarm;
