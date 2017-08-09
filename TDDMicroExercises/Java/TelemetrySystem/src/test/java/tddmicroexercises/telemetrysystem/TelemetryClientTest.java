28e5adf6fe60d36c6a490026f5df08f57124c8aa
Luca Minudel <mymailgateway-github@yahoo.it>
2017-08-09 16:36:13 +0100

    Added missing empty test code.

---
+++ TDDMicroExercises/Java/TelemetrySystem/src/test/java/tddmicroexercises/telemetrysystem/TelemetryClientTest.java
@@ -0,0 +1,13 @@
+package tddmicroexercises.telemetrysystem;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TelemetryClientTest {
+    @Test
+    public void checkTransmission_should_send_a_diagnostic_message_and_receive_a_status_message_response() throws Exception {
+
+    }
+
+}
