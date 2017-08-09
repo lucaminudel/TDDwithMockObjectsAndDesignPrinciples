624a8865ee29240663bb3cd4e81f65d0c09e6c17
Luca Minudel <mymailgateway-github@yahoo.it>
2017-08-09 16:58:43 +0100

    Added empty test to check IDE and unit testing.

---
+++ TDDMicroExercises/Java/TirePressureMonitoringSystem/src/test/java/tddmicroexercises/tirepressuremonitoringsystem/AlarmTest.java
@@ -0,0 +1,12 @@
+package tddmicroexercises.tirepressuremonitoringsystem;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AlarmTest {
+    @Test
+    public void check() throws Exception {
+    }
+
+}
